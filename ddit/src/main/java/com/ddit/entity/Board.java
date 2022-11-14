007792a284995cef954e34d4ea1e8d190d35c892
leprintemps <leprintemps7637@gmail.com>
2022-11-14 20:07:23 +0900

    jpa 추가

---
+++ ddit/src/main/java/com/ddit/entity/Board.java
@@ -0,0 +1,45 @@
+package com.ddit.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity // 객체와 테이블 매핑
+@Table(name = "BOARD") // 테이블 지정
+public class Board {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO INCREMENT 설정 NULL일 경우 자동 생성
+    @Column(name = "board_id")
+    private Long boardId;
+
+    @Column(name = "board_title", nullable = false, columnDefinition = "varchar(255)")
+    private String boardTitle;
+
+    @Column(name = "board_contents", nullable = false, columnDefinition = "varchar(255)")
+    private String boardContents;
+
+    @Column(name = "board_views", nullable = false)
+    private Long boardViews;
+
+    @Column(name = "create_at", nullable = false)
+    private Date createAt;
+    
+    @Column(name = "update_at", nullable = false)
+    private Date updateAt;
+
+    @ManyToOne(targetEntity = Users.class)
+    @JoinColumn(name = "user_id")
+    private Users users;
+
+}
--- ddit/src/main/java/com/ddit/sample/controller/SampleController.java
+++ ddit/src/main/java/com/ddit/sample/controller/SampleController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ddit.sample.vo.SampleVO;
